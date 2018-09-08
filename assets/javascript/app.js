a32f86461b77cb52ea314912eaf83bfc8caa079d
brooke <brooke.mitchell@comcast.net>
2018-09-07 23:21:28 -0400

    pseudo / beginning code

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+var gifSubjects = ["the wonder years","man overboard","the story so far"]
+
+//   Dynamically create buttons for subjects
+
+//When a button is clicked, display 10 static, non-animated gif images from the GIPHY API 
+// Display the rating of each gif underneath
+
+// When a user clicks on a gif, it plays
+// When a user clicks on a gif again, it stops
+
+
+// Allow User To Add Button
+// Submit button
+$("#submit").click(function() {
+    alert( "Handler for .click() called." );
+  });
+// Take the value from the user input box & add it to gifSubjects array
