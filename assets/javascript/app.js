49f631a3dc209313fffbacde75fb39012b4fe9ef
brooke <brooke.mitchell@comcast.net>
2018-09-10 15:51:35 -0400

    code cleanup and style update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,61 +8,67 @@ function createButtons() {
 
   // Loop through the array of gif subjects
   for (var i = 0; i < gifSubjects.length; i++) {
-    // Then dynamicaly generating buttons for each subject in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class
-    a.addClass("subjectButtons");
-    // Adding a data-attribute with a value of the movie at index i
-    a.attr("data-name", gifSubjects[i]);
-    // Providing the button's text with a value of the movie at index i
-    a.text(gifSubjects[i]);
+    // Then dynamicaly generate buttons for each subject in the array.
+    
+    // Set the variable for the button
+    var subjectButton = $("<button>");
+    // Add class to the subject button
+    subjectButton.addClass("subjectButtons");
+    // Add a data-attribute with a value of the gifSubjects array at index i
+    subjectButton.attr("data-name", gifSubjects[i]);
+    // Providing the button's text with a value of the gif subject at index i
+    subjectButton.text(gifSubjects[i]);
     // Adding the button to the HTML
-    $("#buttons").append(a);
+    $("#buttons").append(subjectButton);
   }
 }
 // Call function to create initial buttons
 createButtons();
 
-// This function handles events where one button is clicked
+
+// When the submit button is pressed....
 $("#submit").on("click", function(event) {
   // event.preventDefault() prevents the form from trying to submit itself.
   event.preventDefault();
 
-  // This line will grab the text from the input box
+  // Grab the text from the input box
   var subject = $("#userInput")
     .val()
     .trim();
-  // The movie from the textbox is then added to our array
+
+  // The text from the input box is then added to the gifSubjects array
   gifSubjects.push(subject);
 
+  // call the function to create buttons, this will reload buttons and add your new button
   createButtons();
-  // console.log(subject);
-  // console.log(gifSubjects);
+
+  // clear the input box after submit is clicked
   $(".form-control").val("");
 
 });
 
-createButtons();
-
 
 // Create function for when the buttons are clicked
-// Event listener for buttons
+
+  // When one of the subject buttons is clicked...
   $(document).on('click', '.subjectButtons', function(){ 
-  // console.log("clicked");
-  // In this case, the "this" keyword refers to the button that was clicked
+
+  // Empty out the current gif display
+  $("#gifDisplay").empty();
+
+  // Set the variable for chosenSubject to the subject of the button that was clicked
   var chosenSubject = $(this).attr("data-name");
-  // console.log(chosenSubject);
+
 
   // Display the GIFS
 
   // Call Giphy API
   var queryURL =
     "https://api.giphy.com/v1/gifs/search?q=" +
-    chosenSubject +
+    chosenSubject + 
     "&api_key=YDNLW68rHf5BtaBseXKFI8RgMhB9kl2f&limit=10";
-  // console.log(queryURL);
 
+    // AJAX call
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -70,9 +76,9 @@ createButtons();
     // Storing an array of results in the results variable
     var results = response.data;
 
-    // Looping over every result item
+    // Loop over every result item
     for (var i = 0; i < results.length; i++) {
-      // Creating a div for the gif
+      // Create a div to display the gifs
       var gifDiv = $("<div>");
 
       // Storing the result item's rating
@@ -84,8 +90,7 @@ createButtons();
       gifToDisplay.addClass("theGif");
       
       
-      // Giving the image tag an src attribute of a proprty pulled off the
-      // result item
+      // Giving the image tag an src attribute of a property pulled off the result item
       gifToDisplay.attr("src", results[i].images.fixed_height_still.url);
       gifToDisplay.attr("data-still", results[i].images.fixed_height_still.url);
       gifToDisplay.attr("data-animate", results[i].images.fixed_height.url);
@@ -95,35 +100,31 @@ createButtons();
       // Creating a paragraph tag with the result item's rating
       var p = $("<p>").text("giphy rating: " + rating);
 
- 
-      
-      // gifToDisplay.attr("src", results[i].images.fixed_height.url);
-
-
       // Appending the paragraph and gifToDisplay we created to the "gifDiv" div we created
       gifDiv.append(gifToDisplay);
-      
       gifDiv.append(p);
-      // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+
+      // Prepending the gifDiv to the gifDisplay div in the HTML
       $("#gifDisplay").prepend(gifDiv);
       
-
-      // $(".theGif").on("click", function() {
-
     }
 
+    // Pause / Play gif on click
+
+    // When a gif is clicked...
     $(document).on('click', '.theGif', function(){ 
-      // console.log("clicky");
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  
+      // Set the state variable to the current state of the gif
       var state = $(this).attr("data-state");
-      // console.log(state);
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
+      
+      // If the clicked image's state is still... 
       if (state === "still") {
+        // Update its src attribute to what its data-animate value is.
         $(this).attr("src", $(this).attr("data-animate"));
+        // Then, set the image's data-state to animate
         $(this).attr("data-state", "animate");
-      } else {
+    
+      } else { // Else set src to the data-still value
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
       }
@@ -133,8 +134,3 @@ createButtons();
   });
 });
 
-//When a button is clicked, display 10 static, non-animated gif images from the GIPHY API
-// Display the rating of each gif underneath
-
-// When a user clicks on a gif, it plays
-// When a user clicks on a gif again, it stops
